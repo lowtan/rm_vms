f766ebbd73adb1649872413e8865278fa37fbcfd
Taniro Jang <tan.low@gmail.com>
2026-02-10 15:28:45 +0800

    - [cpp][log] Added mutex for thread safety lockguard.

--- cpp_engine/log/Log.cpp
+++ cpp_engine/log/Log.cpp
@@ -10,10 +10,17 @@
 //     std::cerr << "[cpp_engine][err] " << msg << std::endl;
 // }
 
-void Log::info(const std::string& msg) { 
+void Log::info(const std::string& msg) {
+
+    // Acquire the lock
+    // Only one thread can pass this line at a time. 
+    // Others will wait here until the lock is released.
+    std::lock_guard<std::mutex> lock(s_Mutex);
+
     std::cout << "[cpp_engine] " << msg << std::endl;
 }
 
-void Log::error(const std::string& msg) { 
+void Log::error(const std::string& msg) {
+    std::lock_guard<std::mutex> lock(s_Mutex);
     std::cerr << "[cpp_engine][err] " << msg << std::endl;
 }
--- cpp_engine/log/Log.h
+++ cpp_engine/log/Log.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <mutex>
 
 /**
  * Besides to logging, Log is also used as a way to
@@ -13,4 +14,8 @@ class Log {
 public:
     static void info(const std::string& msg);
     static void error(const std::string& msg);
+
+private:
+    // A single shared lock for the entire application
+    static std::mutex s_Mutex;
 };
